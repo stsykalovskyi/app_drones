f547b9d2a4f7ac242988d524e4067638be19e28f
stsykalovskyi <stsykalovskyi@gmail.com>
2026-02-07 14:36:03 +0000

    fix static in urls file

--- app_drones/urls.py
+++ app_drones/urls.py
@@ -18,20 +18,15 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.static import serve
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-# Serve collected static assets even when DEBUG=False (dev convenience).
-urlpatterns += [
-    re_path(
-        r'^static/(?P<path>.*)$',
-        serve,
-        {'document_root': settings.STATIC_ROOT},
-    )
-]
-
 urlpatterns += [
     path('', include('wiki.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
