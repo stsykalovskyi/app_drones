9881ad34d8ec8687ee1c76290bbdeb46b8fe6188
stsykalovskyi <stsykalovskyi@gmail.com>
2026-02-07 14:25:06 +0000

    add STATIC_DIR param to settings

--- app_drones/settings.py
+++ app_drones/settings.py
@@ -173,6 +173,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_DIR = 'static'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
