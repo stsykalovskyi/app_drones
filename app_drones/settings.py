24573245ac5227231df7f46fbd5d09d18f46a722
stsykalovskyi <stsykalovskyi@gmail.com>
2026-02-07 14:14:45 +0000

    fix csrf origins param set

--- app_drones/settings.py
+++ app_drones/settings.py
@@ -79,6 +79,7 @@ DEBUG = str_to_bool(os.getenv('DEBUG', 'True'))
 
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", default="*").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", default="https://").split(",")
 
 
 
