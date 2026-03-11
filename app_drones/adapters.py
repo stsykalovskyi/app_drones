8fda1f1eee2283ce425e69494b54a6f433052f44
stsykalovskyi <stsykalovskyi@gmail.com>
2026-03-11 16:30:23 +0000

    debug: log social auth errors via CustomSocialAccountAdapter

--- app_drones/adapters.py
+++ app_drones/adapters.py
@@ -1,12 +1,15 @@
-from django.conf import settings
+import logging
+import traceback
+
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+
+logger = logging.getLogger(__name__)
 
 
 class CustomAccountAdapter(DefaultAccountAdapter):
     def save_user(self, request, user, form, commit=True):
         user = super().save_user(request, user, form, commit=False)
-        # user.is_active = False # Will be set by signal
         if commit:
             user.save()
         return user
@@ -20,5 +23,12 @@ class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
         user = super().save_user(request, sociallogin, form)
         return user
 
-
-
+    def on_authentication_error(self, request, provider, error=None, exception=None, extra_context=None):
+        logger.error(
+            "Social auth error | provider=%s | error=%s | exception=%s\n%s",
+            getattr(provider, 'id', provider),
+            error,
+            exception,
+            traceback.format_exc() if exception else '(no traceback)',
+        )
+        super().on_authentication_error(request, provider, error=error, exception=exception, extra_context=extra_context)
