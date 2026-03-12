7d84d2616ec7e0ac60c6dcb401801362e55a8d59
stsykalovskyi <stsykalovskyi@gmail.com>
2026-03-12 10:10:57 +0000

    fix: move ask/ URL before slug pattern in documentation urls

--- documentation/urls.py
+++ documentation/urls.py
@@ -6,10 +6,10 @@ app_name = "documentation"
 
 urlpatterns = [
     path("", views.page_list, name="documentation_page"),
+    path("ask/", views.question_ask, name="question_ask"),
     path("create/", views.page_create, name="page_create"),
     path("category/create/", views.category_create, name="category_create"),
     path("<slug:slug>/", views.page_detail, name="page_detail"),
     path("<slug:slug>/edit/", views.page_edit, name="page_edit"),
     path("<slug:slug>/comment/", views.comment_create, name="comment_create"),
-    path("ask/", views.question_ask, name="question_ask"),
 ]
